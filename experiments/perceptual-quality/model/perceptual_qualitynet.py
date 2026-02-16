0f757395cb1b752cffdb17d4151fbf367da149e9
Jonathan Kron <post.david04@gmail.com>
2026-02-16 13:55:25 +0100

    enable gradient calc for use with percep loss

--- experiments/perceptual-quality/model/perceptual_qualitynet.py
+++ experiments/perceptual-quality/model/perceptual_qualitynet.py
@@ -343,8 +343,8 @@ class PerceptualLoss(nn.Module):
         Returns:
             loss: scalar loss value
         """
-        with torch.no_grad():
-            quality = self.perceptual_net(output_audio)
+        # with torch.no_grad():
+        quality = self.perceptual_net(output_audio)
 
         perceptual_loss = 1.0 - quality.mean()
 
